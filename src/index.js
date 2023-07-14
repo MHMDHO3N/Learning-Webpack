f85ff06dcdd6825659faded6364b33226f86e3d2
mansourlotfi <mansourlotfi@gmail.com>
2023-07-14 22:02:30 +0330

    initializing repo webpack from scratch

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// require("./test.txt");
+
+// require("./style.css");
+// There are two ways to use loaders in your application:
+
+// Configuration (recommended): Specify them in your webpack.config.js file.
+// Inline: Specify them explicitly in each import statement. like below
+
+// It's possible to specify loaders in an import statement, or any equivalent "importing" method. Separate loaders from the resource
+// import Styles from 'style-loader!css-loader?modules!./styles.css';
+
+// note => Prefixing
+// Prefixing with ! will disable all configured normal loaders
+// import Styles from '!style-loader!css-loader?modules!./styles.css';
+
+// Prefixing with !! will disable all configured loaders (preLoaders, loaders, postLoaders)
+
+// Prefixing with -! will disable all configured preLoaders and loaders but not postLoaders
+
+console.warn("Mansour");
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack"); //to access built-in plugins
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  // it could have multiple entry point like this
+  //   entry: {
+  //     main: './path/to/my/entry/file.js',            => Syntax   Usage: entry: { <entryChunkName> string | [string] } | {}
+  //   },
+  //   or pass an array of entry poit like this  known as a "multi-main entry"
+  //   entry: ['./src/file_1.js', './src/file_2.js'],
+
+  //   Multi-Page Application
+  //   entry: {
+  //     pageOne: './src/pageOne/index.js',
+  //     pageTwo: './src/pageTwo/index.js',
+  //     pageThree: './src/pageThree/index.js',
+  //   },
+  //   The object syntax is more verbose
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "my-first-webpack.bundle.js",
+  },
+
+  module: {
+    // rules: [{ test: /\.txt$/, use: "raw-loader" }],
+
+    // use loaders to tell webpack to load a CSS file or to convert TypeScript to JavaScript
+    // npm install --save-dev css-loader ts-loader
+
+    rules: [
+      { test: /\.css$/, use: "css-loader" },
+      { test: /\.ts$/, use: "ts-loader" },
+    ],
+  },
+
+  //   A webpack plugin is a JavaScript object that has an apply method. This apply method is called by the webpack compiler, giving access to the entire compilation lifecycle.
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({ template: "./src/index.html" }),
+    // The ProgressPlugin is used to customize how progress should be reported during compilation,
+    // and HtmlWebpackPlugin will generate a HTML file including the my-first-webpack.bundle.js file using a script tag.
+  ],
+};
